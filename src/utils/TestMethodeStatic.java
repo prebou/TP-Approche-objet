b90f7f20dd195924364aa0dcb262958f566d3313
parameswaran <prebou1999@gmail.com>
2024-10-07 11:23:17 +0200

    tp 4

---
+++ src/utils/TestMethodeStatic.java
@@ -0,0 +1,18 @@
+package utils;
+import javax.lang.model.type.TypeVariable;
+import java.lang.Integer;
+
+
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "12";
+        int entier = Integer.parseInt(chaine);
+        int a = 1, b =10;
+        System.out.println(entier);
+
+        int maximum = Integer.max(a, b);
+        System.out.println(maximum);
+
+    }
+}
