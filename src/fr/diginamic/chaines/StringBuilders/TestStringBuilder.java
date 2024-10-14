742f6b8c67c0807eaac055dfaea6e7a3ad2d3e66
parameswaran <prebou1999@gmail.com>
2024-10-14 11:02:03 +0200

    TP 16 StringBuilder

---
+++ src/fr/diginamic/chaines/StringBuilders/TestStringBuilder.java
@@ -0,0 +1,17 @@
+package fr.diginamic.chaines.StringBuilders;
+
+public class TestStringBuilder {
+    public static void main(String[] args) {
+        //StringBuilder builder = new StringBuilder();
+        String chaine = "";
+        long debut = System.currentTimeMillis();
+        for (int i = 0; i < 100_000; i++) {
+            //builder.append(i); // resultat 8ms
+            chaine = i +" "+ i; // 496ms
+            System.out.println(chaine);
+
+        }
+        long fin = System.currentTimeMillis();
+        System.out.println("Temps écoulé en millisecondes :" + (fin - debut));
+    }
+}
